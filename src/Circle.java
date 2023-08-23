ec08ff79cd0fa9f09d18eef87386fba6f3477654
liber09 <linda.bergsangel@hotmail.com>
2023-08-23 04:37:24 +0200

    Basic class structure for rectangle and circle

---
+++ src/Circle.java
@@ -0,0 +1,11 @@
+public class Circle {
+    double diameter;
+
+    public double getDiameter() {
+        return diameter;
+    }
+
+    public void setDiameter(double value){
+        this.diameter = value;
+    }
+}
---
+++ src/Rectangle.java
@@ -0,0 +1,21 @@
+public class Rectangle {
+
+    private double width;
+    private double height;
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+}
